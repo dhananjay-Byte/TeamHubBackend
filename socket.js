f94f9501708058b7df9ac50814012da2ad28f222
Dhananjay <82323909+dhananjay-Byte@users.noreply.github.com>
2025-02-02 17:55:49 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -7,7 +7,7 @@ let io;
 const initializeSocket = (server) => {
   io = socketIo(server, {
     cors: {
-      origin:['http://localhost:3000'],
+      origin:["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
       methods: ["GET", "POST"],
     },
   });
