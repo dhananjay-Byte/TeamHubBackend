60c43d10b52c7dd4a7e3f34d803d757363799fa7
Dhananjay <82323909+dhananjay-Byte@users.noreply.github.com>
2025-02-02 13:44:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
     credentials: true,
   })
 );
