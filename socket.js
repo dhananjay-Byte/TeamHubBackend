c99a4b276a33ec0b65cbb3889528a9b5f20f476f
Dhananjay <82323909+dhananjay-Byte@users.noreply.github.com>
2025-02-02 15:16:44 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -7,7 +7,7 @@ let io;
 const initializeSocket = (server) => {
   io = socketIo(server, {
     cors: {
-      origin:['http://localhost:3000','http://localhost:3002'],
+      origin:['http://localhost:3000/chat','http://localhost:3002'],
       methods: ["GET", "POST"],
     },
   });
